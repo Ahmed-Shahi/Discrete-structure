7ddcc4c5917078ff74379025f5edc5efdb8f1f9a
Ahmed-Shahi <ahmedshahi038@gmail.com>
2023-10-07 17:40:31 +0500

    ok

---
+++ Euler formula/app.js
@@ -0,0 +1,20 @@
+// let a = document.getElementById('V')
+// let b = document.getElementById('D')
+
+// let btn = () => {
+//     let v = a
+//     console.log(v)
+//     let d = a
+//     console.log(d)
+    
+// }
+
+function calculate(){
+    let a = document.getElementById('vertices').value
+    let b = document.getElementById('degree').value
+    
+    // let v = a.value
+    console.log(a)
+    // let d = b.value
+    console.log(b)
+}
